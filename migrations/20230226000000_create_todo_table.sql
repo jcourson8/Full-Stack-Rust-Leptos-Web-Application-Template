74b0a142ae9e36ca7268c531048d637909fa21e7
James Courson <jamescourson@jamess-mbp.lan>
2023-07-11 09:57:08 -0500

    updated migration sql to have uuid instead of integer id

--- migrations/20230226000000_create_todo_table.sql
+++ migrations/20230226000000_create_todo_table.sql
@@ -1,27 +1,54 @@
+-- Updated with Uuid for ID'ss
 CREATE TABLE IF NOT EXISTS users (
-  id         INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+  id         TEXT NOT NULL PRIMARY KEY,
   username   TEXT NOT NULL UNIQUE,
   password   TEXT NOT NULL,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE IF NOT EXISTS user_permissions (
-    user_id  INTEGER NOT NULL,
-    token    TEXT NOT NULL
+    user_id  TEXT NOT NULL,
+    token    TEXT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
-
--- INSERT INTO users (id, anonymous, username, password) 
--- SELECT 0, true, 'Guest', ''
--- ON CONFLICT(id) DO UPDATE SET
---     anonymous = EXCLUDED.anonymous,
---     username = EXCLUDED.username;
-
 
 CREATE TABLE IF NOT EXISTS todos (
-  id         INTEGER PRIMARY KEY AUTOINCREMENT,
-  user_id    INTEGER NOT NULL,
+  id         TEXT PRIMARY KEY,
+  user_id    TEXT NOT NULL,
   title      TEXT NOT NULL,
   completed  BOOLEAN,
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-  -- FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
+
+
+
+-- CREATE TABLE IF NOT EXISTS users (
+--   id         INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+--   username   TEXT NOT NULL UNIQUE,
+--   password   TEXT NOT NULL,
+--   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+-- );
+
+-- CREATE TABLE IF NOT EXISTS user_permissions (
+--     user_id  INTEGER NOT NULL,
+--     token    TEXT NOT NULL
+-- );
+
+-- -- INSERT INTO users (id, anonymous, username, password) 
+-- -- SELECT 0, true, 'Guest', ''
+-- -- ON CONFLICT(id) DO UPDATE SET
+-- --     anonymous = EXCLUDED.anonymous,
+-- --     username = EXCLUDED.username;
+
+
+-- CREATE TABLE IF NOT EXISTS todos (
+--   id         INTEGER PRIMARY KEY AUTOINCREMENT,
+--   user_id    INTEGER NOT NULL,
+--   title      TEXT NOT NULL,
+--   completed  BOOLEAN,
+--   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+--   -- FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+-- );
+
+
